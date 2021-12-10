a171b3674b9d388379e2d27b88c956c2eaefb942
jccallo <jccallo@outlook.com>
2021-12-10 10:49:53 -0500

    creamos el product.json y la clase product entry

---
+++ app/src/main/java/pe/jccallo/themouseapp/application/TheMouseAppApplication.java
@@ -0,0 +1,26 @@
+package pe.jccallo.themouseapp.application;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class TheMouseAppApplication extends Application {
+    private static TheMouseAppApplication instance;
+    private static Context appContext;
+
+    public static  TheMouseAppApplication getInstance() {return instance; }
+
+    public static Context getAppContext() { return appContext; }
+
+    public void setAppContext(Context mAppContext) { this.appContext = mAppContext; }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        this.setAppContext(getApplicationContext());
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+}
